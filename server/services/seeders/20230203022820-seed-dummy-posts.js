cf3db9e76039b17ce445056281903a62c7125543
arsa0 <arsafauzi@gmail.com>
2023-02-04 06:53:24 +0700

    done all posts test and controller

---
+++ server/services/seeders/20230203022820-seed-dummy-posts.js
@@ -0,0 +1,32 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    const data = require("../db.json").posts;
+
+    await queryInterface.bulkInsert("Posts", data);
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete("Posts", null, {
+      truncate: true,
+      restartIdentity: true,
+    });
+  },
+};
