2af03ebe4b05bca2c179efb6c9c65a23a3b2bf67
whooptywoo <albertusrhz@gmail.com>
2023-02-08 14:39:57 +0700

    Add Include at Bids

--- services/models/user.js
+++ services/models/user.js
@@ -18,6 +18,8 @@ module.exports = (sequelize, DataTypes) => {
 				foreignKey: "reportedId",
 				as: "Reported",
 			});
+			User.hasMany(models.Bid, {});
+			// User.hasMany(models.Post, {});
 		}
 	}
 	User.init(
